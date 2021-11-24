200f7af6d374ef591a6f35f1917145da62b31ecc
Febi Mudiyanto <62255283+febimudiyanto@users.noreply.github.com>
2021-11-24 07:01:37 +0700

    Add files via upload

---
+++ python/socketModule/client.py
@@ -0,0 +1,10 @@
+#date = 24-11-2021
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((socket.gethostname(),9337))
+msg = sock.recv(1024) 
+sock.close()
+print(msg.decode("ascii"))
+
