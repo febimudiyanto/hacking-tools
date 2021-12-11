256f9482a82372f367b92565c0cf062098f69038
Febi Mudiyanto <62255283+febimudiyanto@users.noreply.github.com>
2021-12-11 20:43:58 +0700

    add time and looping receive

--- python/socketModule/client.py
+++ python/socketModule/client.py
@@ -1,10 +1,13 @@
 #date = 24-11-2021
-
 import socket
+import datetime
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((socket.gethostname(),9337))
-msg = sock.recv(1024) 
+print("connect with ",socket.gethostname())
+while 1:
+    msg = sock.recv(1024) 
+    if not msg:break
+    now = datetime.datetime.now()
+    print("["+now.strftime('%Y-%m-%d %H:%M:%S')+"] ",msg.decode("ascii"))
 sock.close()
-print(msg.decode("ascii"))
-
